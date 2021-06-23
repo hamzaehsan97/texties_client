9bf99d6fb02498ca7e18c705cb2d6f0cb9e33c60
Hamza Ehsan <hamza.ehsan@lawrence.edu>
2021-06-23 18:56:02 -0500

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import UserContext from "../components/UserContext";
-
 export default (req, res) => {
   res.status(200).json({ name: "John Doe" });
 };
