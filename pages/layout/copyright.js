f57f927ddcf38e1807e4942af4252aae7a7922ec
Hamza Ehsan <hamzaehsan75@gmail.com>
2021-08-23 11:05:10 -0500

    Revised portfolio link

--- pages/layout/copyright.js
+++ pages/layout/copyright.js
@@ -5,7 +5,7 @@ export default function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="https://hamzaehsan97.github.io/Portfolio//">
         Hamza Ehsan & Co.
       </Link>{" "}
       {new Date().getFullYear()}
