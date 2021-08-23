91cd6be670d01ffda6221f7e7e9dab8fd98e495e
Hamza Ehsan <hamzaehsan75@gmail.com>
2021-08-22 23:04:48 -0500

    Title added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
 import Container from "@material-ui/core/Container";
 import LoginForm from "../components/LoginForm";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <Container component="main" maxWidth="xs">
+      <Head>
+        <title>Texties: Storage through SMS</title>
+      </Head>
       <LoginForm />
     </Container>
   );
