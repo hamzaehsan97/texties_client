8bad66bef7f52127e181fca76c636cff475dd5db
hamzaehsan97 <hamzaehsan75@gmail.com>
2021-10-28 21:49:24 -0500

    Signup feature on homepafe

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,31 @@ import { useRouter } from "next/router";
 import UserContext from "../components/UserContext";
 import Image from "next/image";
 import styles from "../styles/index.module.css";
-
+import { Button, TextField } from "@material-ui/core";
+import { LinearProgress } from "@material-ui/core";
 export default function Home() {
+  const [phone_number, setPhone_Number] = useState("");
+  const [signupErrors, setSignupErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = (e) => {
+    if (phone_number.length === 0 || phone_number.length < 10) {
+      setSignupErrors(["Please enter your phone number correctly"]);
+      return;
+    }
+    setLoading(true);
+    axios
+      .post("https://texties.herokuapp.com/signup?phone_number=" + phone_number)
+      .then((res) => {
+        setSignupErrors(null);
+        setLoading(false);
+        window.location.href = "/login";
+      })
+      .catch((err) => {
+        setSignupErrors(err.response.data["Error"]);
+        setLoading(false);
+      });
+  };
+
   return (
     <Grid
       container
@@ -32,6 +55,52 @@ export default function Home() {
             <Typography variant="h6" className={styles.sub_title}>
               Texties helps you keep track of your notes. Sign-up today!
             </Typography>
+          </Grid>
+          <Grid item>
+            <form>
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing={1}
+                className={styles.form}
+              >
+                <Grid item>
+                  <TextField
+                    variant="outlined"
+                    value={phone_number}
+                    name="phone_number"
+                    label="Phone Number"
+                    type="phone_number"
+                    id="phone_number"
+                    onChange={(e) => setPhone_Number(e.target.value)}
+                    style={{ maxHeight: 55 + "px", background: "#fff" }}
+                  />
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    onClick={handleSubmit}
+                    className={styles.access_button}
+                  >
+                    Get Access
+                  </Button>
+                </Grid>
+                <Grid item>
+                  {loading ? <LinearProgress /> : null}
+                  {signupErrors ? (
+                    <Typography
+                      variant="subtitle1"
+                      className={styles.errorText}
+                      gutterBottom
+                    >
+                      {signupErrors}
+                    </Typography>
+                  ) : null}
+                </Grid>
+              </Grid>
+            </form>
           </Grid>
         </Grid>
       </Grid>
